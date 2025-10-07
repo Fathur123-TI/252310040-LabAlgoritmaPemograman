5ae61a7157bcfaf508aac38bb89ceb6a24d2990c
Fathur123-TI <fr6265488@gmail.com>
2025-10-07 13:45:11 +0700

    Add files via upload

---
+++ Pertemuan 2/5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define harga 4500.02 //
+
+int main()
+{
+	float jumlah,total;
+	
+	cout << "Masukan jumlah barang = " ;
+	cin >> jumlah;
+	total = harga * jumlah;
+	cout<<"nYang harus dibayar = "<<total;
+
+}
