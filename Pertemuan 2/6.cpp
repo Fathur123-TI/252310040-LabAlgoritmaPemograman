7b2452e476cefb8fda099bb5d73c1d354e47d81e
Fathur123-TI <fr6265488@gmail.com>
2025-10-30 13:18:19 +0700

    Add files via upload

---
+++ Pertemuan 2/6.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	
+	cout << "Nomor 1 - Penjumlahan" << endl;
+	cout << "Masukkan 2 angka" << endl;
+	
+	cout << "Nilai A = ";
+	cin >> a;
+	cout << "\nNilai B = ";
+	cin >> b;
+	cout << a << " + " << b << " = "<<a+b<<endl;
+	cout << "Hasil A + B = "<<a+b<<endl;
+	
+	cout << "Nomor 2 - Pengurangan" << endl;
+	cout << "Masukkan 2 angka" << endl;
+	
+	cout << "Nilai A = ";
+	cin >> a;
+	cout << "\nNilai B = ";
+	cin >> b;
+	cout << a << " - " << b << " = "<<a-b<<endl;
+	cout << "Hasil A - B = "<<a-b<<endl;
+	
+	cout << "Nomor 3 * Perkalian" << endl;
+	cout << "Masukkan 2 angka" << endl;
+	
+	cout << "Nilai A = ";
+	cin >> a;
+	cout << "\nNilai B = ";
+	cin >> b;
+	cout << a << " * " << b << " = " <<a*b<<endl;
+	cout << "Hasil  A * B = "<<a*b<<endl;
+	
+	return 0;
+	
+}
---
+++ Pertemuan 2/tugas 2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+
+    // Penjumlahan
+    cout << "Nomor 1 - Penjumlahan" << endl;
+    cout << "Masukkan 2 angka" << endl;
+    cout << "A = ";
+    cin >> A;
+    cout << "B = ";
+    cin >> B;
+    cout << "A + B = " << A + B << endl;
+    cout << "Hasil A + B = " << A + B << endl << endl;
+
+    // Pengurangan
+    cout << "Nomor 2 - Pengurangan" << endl;
+    cout << "Masukkan 2 angka" << endl;
+    cout << "A = ";
+    cin >> A;
+    cout << "B = ";
+    cin >> B;
+    cout << "A - B = " << A - B << endl;
+    cout << "Hasil A - B = " << A - B << endl << endl;
+
+    // Perkalian
+    cout << "Nomor 3 - Perkalian" << endl;
+    cout << "Masukkan 2 angka" << endl;
+    cout << "A = ";
+    cin >> A;
+    cout << "B = ";
+    cin >> B;
+    cout << "A * B = " << A * B << endl;
+    cout << "Hasil A * B = " << A * B << endl;
+
+    return 0;
+}
+
